c5ec2520026397bd2bb3d3c9db0512500beacb22
MatheusOliveira04 <samatheus999@gmail.com>
2025-07-31 21:09:02 -0300

    feat: create initial S3 project

---
+++ src/main/java/com/matheusoliveira04/s3flow/S3flowApplication.java
@@ -0,0 +1,13 @@
+package com.matheusoliveira04.s3flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3flowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3flowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheusoliveira04/s3flow/S3flowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusoliveira04.s3flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3flowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
