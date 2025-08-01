829751866cb1542bd1717b1aac9c86d4adc7c502
MatheusOliveira04 <samatheus999@gmail.com>
2025-08-01 19:40:20 -0300

    feat: add S3Controller for file download to AWS S3

--- src/main/java/com/matheusoliveira04/s3flow/controller/S3Controller.java
+++ src/main/java/com/matheusoliveira04/s3flow/controller/S3Controller.java
@@ -1,10 +1,9 @@
 package com.matheusoliveira04.s3flow.controller;
 
 import com.matheusoliveira04.s3flow.service.S3Service;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -22,6 +21,14 @@ public class S3Controller {
     public ResponseEntity<String> upload(@RequestParam MultipartFile file) throws IOException {
         s3Service.uploadFile(file);
         return ResponseEntity.ok("File uploaded successfully");
+    }
+
+    @GetMapping("/download/{filename}")
+    public ResponseEntity<byte[]> download(@PathVariable String filename) {
+        byte[] data = s3Service.downloadFile(filename);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + filename)
+                .body(data);
     }
 
 }
