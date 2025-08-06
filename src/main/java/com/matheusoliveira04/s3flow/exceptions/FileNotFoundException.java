5c4f1dcc1a669cdc45a5aaaa4ac79a9e1e147104
MatheusOliveira04 <samatheus999@gmail.com>
2025-08-06 20:36:14 -0300

    feat: create FileNotFoundException exception class

---
+++ src/main/java/com/matheusoliveira04/s3flow/exceptions/FileNotFoundException.java
@@ -0,0 +1,8 @@
+package com.matheusoliveira04.s3flow.exceptions;
+
+public class FileNotFoundException extends RuntimeException {
+
+    public FileNotFoundException(String message) {
+        super(message);
+    }
+}
